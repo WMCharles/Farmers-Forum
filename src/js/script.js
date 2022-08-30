bc1a9e1c9d8fd155353583b1f7f35ac4ec4c3fb0
WMCharles <charles.wafula@student.moringaschool.com>
2022-08-30 12:20:02 +0300

    add icons

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,43 @@
-function fetchPost(){
+//Fetch Posts
+function fetchPosts(){
     fetch("http://localhost:3000/posts")
-    .then(response => response.json())
+    .then(res => res.json())
     .then(posts => posts.forEach(post => renderPost(post)))
 }
 
+//Render Posts
 function renderPost(post){
     let content = document.createElement("div")
     content.className = "content"
-    content.innerHTML = `
-        <h3>${post.title}</h3>
+    content.innerHTML = 
+    `   <h3><i class="fa-regular fa-user"></i> - ${post.title}</h3>
         <p>
             ${post.body}
         </p>
         <div class="icons">
-            <p><span><i class="fa-regular fa-thumbs-up"></i></span> ${post.likes} Likes</p>
-            <p></i> <i class="fa-regular fa-message"></i> 20 Comments</p>
+            <div class="likes">
+                <p>
+                    <i class="fa-regular fa-thumbs-up"></i></span> ${post.likes} likes
+                </p>
+                <p>
+                    <i class="fa-regular fa-thumbs-down"></i> ${post.dislikes} dislikes
+                </p>
+                <p>
+                    <i class="fa-regular fa-message"></i> Comments
+                </p>
+            </div>
+            <div>
+                <p>
+                    Share <i class="fa-regular fa-share-from-square"></i>
+                </p>
+            </div>
         </div>
     `
-    document.getElementById("contents").appendChild(p)
+    document.querySelector(".contents").appendChild(content)
 }
 
+//Display Posts
 function displayPost(){
-    fetchPost()
+    fetchPosts()
 }
+displayPost()
